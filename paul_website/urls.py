149514c42c85ea465947c68b2dc5fc959f11f46d
uger paul <ugerpaul5@gmail.com>
2018-06-12 09:45:14 +0300

    changes made to settings,urls

--- paul_website/urls.py
+++ paul_website/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from my_personal_app import urls
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'paul_website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+     url(r'', include('my_personal_app.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
